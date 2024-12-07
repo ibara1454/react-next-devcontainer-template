1c4b180895c493d9d01c4e1cc497f4e7ac141d38
Chiajun Wang <ibara1454@gmail.com>
2024-12-07 17:49:25 +0000

    feat: init

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+export default {
+  plugins: [
+    // Automatically sorts tailwind classes
+    // https://github.com/tailwindlabs/prettier-plugin-tailwindcss
+    "prettier-plugin-tailwindcss",
+  ],
+};
